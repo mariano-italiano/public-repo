f378e29222944837af5a7854af9995b94fd6a07c
mariano-italiano <77984926+mariano-italiano@users.noreply.github.com>
2021-01-28 14:01:39 +0100

    Update skrypt8.sh

--- rhcsa/scripts/skrypt8.sh
+++ rhcsa/scripts/skrypt8.sh
@@ -18,21 +18,3 @@ rm -f $2;
 };;
 *) echo "Użycie: $0 <opcja> nazwa_pliku";
 esac
-
-Skrypt8:
-
-#!/bin/bash
-# pętle for
-
-for i in 1 2 3 4 5
-do
-
-echo $i;
-
-done
-
-for i in skrypt*
-do
-
-echo $i;
-done
