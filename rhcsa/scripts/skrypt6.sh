8da7d9be3aa21c01cbfad06b131a00a7b9031c70
mariano-italiano <77984926+mariano-italiano@users.noreply.github.com>
2021-01-28 13:59:47 +0100

    Create skrypt6.sh

---
+++ rhcsa/scripts/skrypt6.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#	zmienne i zmienne specjalne
+###########################################################
+
+#	zmienna
+zmienna1=2;
+
+zmienna2=3;
+suma1=$[$zmienna1 + $zmienna2];
+
+let suma2=zmienna1+zmienna2;
+echo $suma1;
+
+echo $suma2;
+###########################################################
+
+#	zmienne specjalne
+
+#	nazwa biez±cego skryptu: $0 echo $0;
+
+#	argumenty skryptu: $1 - $9 echo "argument 1: $1"; echo "argument 2: $2";
+#	wszystkie argumenty: $@
+
+echo "wszystkie argumenty: $@";
+# kod powrotu ostatnio wydanego polecenia: $?
+
+echo "kod powrotu ostatnio wydanego polecenia: $?";
+
+#	generujemy bł±d lssfsdfsdff 2> /dev/null
+echo "Czy jest bł±d (1)? : $?";
+
+#	PID bież±cej powłoki: $$ echo "PID powłoki: $$";
