826e675b4fbaeaa2232358917dc14a8c3a4f35f2
mariano-italiano <77984926+mariano-italiano@users.noreply.github.com>
2021-01-28 14:00:11 +0100

    Create skrypt5.sh

---
+++ rhcsa/scripts/skrypt5.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#	zmienne ¶rodowiskowe echo $HOME;
+
+echo $USER; echo $HOSTNAME;
+
+export set ZMIENNA_NOWA="nowa zmienna ¶rodowiskowa"; echo $ZMIENNA_NOWA;
+
+#	zmienne tablicowe
+tablica1=(1 2 3 4);
+
+tablica2=(jeden dwa trzy);
+echo ${tablica1[0]};
+
+echo ${tablica2[2]};
+unset tablica1[2];
+
+echo ${tablica1[@]};
+
+unset tablica2[@];
+echo ${tablica2[@]};
